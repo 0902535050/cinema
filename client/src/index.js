bf19fd4737cec705f48ff76ea120dd22d8a752a9
Pham Tuan <tuanphamthanh903@gmail.com>
2022-06-01 16:24:24 +0700

    remove context global api watchList

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./authContext/AuthContext";
-import { GlobalProvider } from "./context/GlobalState";
-
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <GlobalProvider>
-        <App />
-      </GlobalProvider>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
